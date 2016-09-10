a488cf11d2d512007e9aae3415e6d3ba42ab38e1
AkeEvoX <svargalok@gmail.com>
2016-09-10 23:14:14 +0700

    edit index and revise menu

--- js/utility.js
+++ js/utility.js
@@ -7,3 +7,15 @@ $(function(){
 $('.tree-toggle').parent().children('ul.tree').toggle(200);
 })
 */
+
+function getParameterByName(name, url) {
+	
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+	
+}
