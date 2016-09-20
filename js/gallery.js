40f762b945c72024a98971eef74c7cfdd261f176
AkeEvoX <svargalok@gmail.com>
2016-09-20 08:39:56 +0700

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,13 +1,13 @@
 function loadList()
 {
-	var service = 'services/gallery' ;
+	var service = '../service/sanitary/gallery' ;
 	var data = {"_": new Date().getMilliseconds()}
 	CallService(service,data,setview);
 }
 
 function LoadItem(id)
 {
-	var service = 'services/gallery' ;
+	var service = '../service/sanitary/gallery' ;
 	var data = {"_": new Date().getMilliseconds(),"id":id}
 	CallService(service,data,setviewdetail);
 }
