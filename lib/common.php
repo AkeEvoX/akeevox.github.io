ed704015be57669cde3b6aaf89e065809690656d
AkeEvoX <werawat.l@icloud.com>
2016-10-31 08:30:34 +0700

    update link from logo

--- lib/common.php
+++ lib/common.php
@@ -1,7 +1,7 @@
 <?php 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ERROR);
+error_reporting(E_ERROR|E_PARSE);
 /*E_PARSE | E_WARNING | E_NOTICE , E_ALL*/
 
 $lang="en";
