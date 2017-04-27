2760dac7356f9359c6bb6d0153c3a8f055fac72c
AkeEvoX <svargalok@gmail.com>
2017-04-27 22:50:29 +0700

    add new page dealer administrator

---
+++ admin/js/dealer-manager.js
@@ -0,0 +1,144 @@
+
+var dealer = {};
+
+dealer.add = function(args){
+	
+	var endpoint = "services/dealer.php";
+	var method = "POST";
+	utility.data(endpoint,method,args,function(data){
+		
+		
+		var response = JSON.parse(data);
+		console.debug(response);
+		alert(response.result);
+		control.pagetab('dealer-manager.html');
+	});
+
+}
+
+
+dealer.edit = function(args){
+	
+	var endpoint = "services/dealer.php";
+	var method = "POST";
+	utility.data(endpoint,method,args,function(data){
+		
+		
+		var response = JSON.parse(data);
+		console.debug(response);
+		alert(response.result);
+		control.pagetab('dealer-manager.html');
+	});
+
+}
+
+dealer.delete = function(){
+	
+	console.log('call delete');
+	var endpoint = "services/dealer.php";
+	var method = "POST";
+	var args = "";
+	 $('input[name="mark[]"]:checked').each(function(){
+		 
+		 var id = $(this).attr('data-id');
+		 
+		 args =  {'_':new Date().getMilliseconds(),'type':'del' , 'id':id};
+		 utility.service(endpoint,method,args,function(){
+			$('#row'+id).remove();	 
+		 });
+		 
+		 console.log('delete id='+id);
+	 });
+	 
+	 alert('DELETE SUCCESS.');
+	 dealer.loadlist();
+}
+
+dealer.reset = function(){
+	
+	$('#title_th').val('');
+	$('#title_en').val('');
+}
+
+dealer.loaditem = function(id){
+	
+	$('#id').val(id);
+	var endpoint = "services/dealer.php";
+	var method = "GET";
+	var args = {'_':new Date().getMilliseconds(),'type':'item','id':id};
+	utility.service(endpoint,method,args,set_view_item);
+	
+}
+
+dealer.load = function(){
+	
+	var endpoint = "services/dealer.php";
+	var method = "GET";
+	var args = {'_':new Date().getMilliseconds(),'type':'page'};
+	utility.service(endpoint,method,args,set_view);
+	
+}
+
+dealer.loadlist = function(){
+	var endpoint = "services/dealer.php";
+	var method = "GET";
+	var args = {'_':new Date().getMilliseconds(),'type':'list' ,'couter':$('#counter').val(),'fetch':'20'  };
+	utility.service(endpoint,method,args,set_view_list);
+}
+
+function set_view(data){
+	
+	console.log(data);
+	
+	if(data.result==undefined) return;
+	
+	//var item = data.result;	
+	gallery.data = data.result;
+	
+	$('#title_th').val(personal.data["org.title"].th);
+	$('#title_en').val(personal.data["org.title"].en);
+}
+
+function set_view_item(data){
+	
+	if(data.result==undefined) return;
+	
+	
+	$('#title_th').val(data.result.title_th);
+	$('#title_en').val(data.result.title_en);
+	$('#album_type').val(data.result.album_id);
+	$('#preview').attr('src',"../"+data.result.thumbnail);
+	
+	if(data.result["active"]=="1")
+		$('#active').prop('checked',true);
+	
+}
+
+function set_view_list(data){
+	//console.debug(data);
+	var view = $('#data_list');
+	var item = "";
+	if(data.result==undefined || data.result=="") {
+		console.log("dealer-manager > list :: data not found.")
+		return;
+	}
+	var max_item = $('#counter').val();
+	$.each(data.result,function(i,val){
+		var param = '?id='+val.id;
+		var active = val.active == "1" ? "<span class='btn btn-success btn-sm'>Enable</span> ": "<span class='btn btn-danger btn-sm'>Disable</span>";
+		var date = val.update_date == null ? val.create_date : val.update_date;
+		
+		item+="<tr id='row"+val.id+"'>";
+		item+="<td><input type='checkbox' name='mark[]' data-id='"+val.id+"' /></td>";
+		item+="<td>"+val.id+"</td>";
+		item+="<td><img src='../"+val.thumbnail+"' class='img-responsive' /></td>";
+		item+="<td>"+val.album_th+"</td>";
+		item+="<td>"+date+"</td>";
+		item+="<td>"+ active +"</td>";
+		item+="<td><span class='btn btn-warning btn-sm' onclick=control.pagetab('dealer-edit.html','"+param+"') >แก้ไข</span></td>";
+		item+="</tr>";
+		max_item++;
+	});
+	$('#counter').val(max_item);
+	view.append(item);
+}
---
+++ admin/services/dealer.php
@@ -0,0 +1,150 @@
+<?php
+session_start();
+include("../../lib/common.php");
+include("../../lib/logger.php");
+//$source_globle = "../../";
+$base_dir = "../../";
+include("../../controller/DealerManager.php");
+
+$id="";
+$type="";
+
+$id=GetParameter("id");
+$type=GetParameter("type");
+
+$result = "";
+
+switch($type){
+	case "list":
+		$counter = $_GET["couter"];// count last fetch data
+		$max_fetch = $_GET["fetch"];
+		$result = get_list_fetch($counter,$max_fetch);
+	break;
+	
+	case "add":
+		$result = Insert($_POST);
+		log_debug("dealer  > Insert " . print_r($result,true));
+	break;
+	
+	case "edit":
+		$result = Update($_POST);
+		log_debug("dealer > Update " . print_r($result,true));
+	break;
+	case "del":
+		$result = Delete($id);
+	break;
+	case "item":
+		$result = get_Items($id);
+	break;
+	default:
+		/*other implement here.*/
+	break;
+}
+
+
+echo json_encode(array("result"=> $result ,"code"=>"0"));
+
+/************* function list **************/
+function get_list_fetch($start_fetch,$max_fetch){
+	
+	$dealer = new DealerManager();
+	$data = $dealer->get_fetch_list($start_fetch,$max_fetch);
+	$result = "";
+	
+	if($data==null) return $result;
+	
+	
+	while($row = $data->fetch_object()){
+
+			$result[] = $row;
+	}
+	return $result;
+	
+}
+
+function get_Items($id){
+	
+	$dealer = new DealerManager();
+	$data = $dealer->get_dealer_info($id);
+	
+	if($data){
+		
+			$result = $data->fetch_object();
+		
+	}
+	return $result;
+}
+
+function get_Item_album($id){
+	
+	$dealer = new DealerManager();
+	$data = $dealer->get_album_info($id);
+	
+	if($data){
+		
+			$result = $data->fetch_object();
+		
+	}
+	return $result;
+}
+
+function Insert($items){
+	
+	if($_FILES['file_upload']['name']!=""){
+		$filename = "images/gallery/".$_FILES['file_upload']['name'];
+		$distination =  "../../".$filename;
+		$source = $_FILES['file_upload']['tmp_name'];  
+		$items["image"] = $filename;
+		$items["thumbnail"] = $filename;
+	}
+
+	$dealer = new DealerManager();
+	//1)insert data
+	$result = $dealer->insert_item($items);
+	//2)upload image personal
+	if($items["thumbnail"])
+		upload_image($source,$distination);
+	
+	return "INSERT SUCCESS.";
+}
+
+
+function Update($items){
+	
+	$items["link_th"] = "";
+	$items["link_en"] = "";
+	
+	if($_FILES['file_upload_th']['name']!=""){
+		$filename = "images/dealer/th_". date('Ymd_His') ."_".$_FILES['file_upload_th']['name'];
+		$distination =  "../../".$filename;
+		$source = $_FILES['file_upload_th']['tmp_name'];
+		$items["link_th"] = $filename;
+	}
+	if($_FILES['file_upload_en']['name']!=""){
+		$filename = "images/dealer/en_". date('Ymd_His') ."_".$_FILES['file_upload_en']['name'];
+		$distination =  "../../".$filename;
+		$source = $_FILES['file_upload_en']['tmp_name'];
+		$items["link_en"] = $filename;
+	}
+	
+	$dealer = new DealerManager();
+	//1)update data
+	$result = $dealer->update_item($items);
+	
+	//2)upload image
+	if($items["thumbnail"])
+		upload_image($source,$distination);
+	
+	return "UPDATE SUCCESS.";
+	
+}
+
+
+function Delete($id){
+	
+	$dealer = new DealerManager();
+	$dealer->delete_item($id);
+	return "DELETE SUCCESS.";
+}
+
+?>
